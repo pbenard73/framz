8cee60ffff7db27bb88680757a5b72a052aca547
BENARD Patrick <pbenard73@gmail.com>
2020-10-18 16:05:20 +0200

    exportModule

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import baseAclManager from './core/managers/acl'
 import baseCacheManager from './core/managers/cache'
 import baseCrypter from './core/helpers/crypter'
 import baseDatabase from "./core/database/database"
+import baseModel from "./core/database/baseModel"
 import baseExpress from "express"
 import baseSequelize from "sequelize"
 import baseModule from './core/thirds/module'
@@ -18,6 +19,7 @@ export const aclManager = baseAclManager
 export const cache = baseCacheManager
 export const crypter = baseCrypter
 export const database = baseDatabase
+export const Model = baseModel
 export const Module = baseModule
 export const createError = baseCreateError
 export const express = baseExpress
